aee723f106a97aac8acbbbd27e02932d3441cbc4
rento <rentosaiko@gmail.com>
2024-02-29 22:01:43 +0900

    調整

--- Assets/Tajiri/Assets/scripts/Menu/MusicSelectButton.cs
+++ Assets/Tajiri/Assets/scripts/Menu/MusicSelectButton.cs
@@ -6,12 +6,12 @@ using TMPro;
 
 public class MusicSelectButton : MonoBehaviour
 {
-    public TextMeshProUGUI targetText;//テキストメッシュプロにはこれが必要
-    public string newText; //曲名を表示
-    public Image targetImage;//ターゲットとなる画像。これの中身を変える
-    public Sprite newSprite;//新しい中身
+    public TextMeshProUGUI targetText;
+    public string newText; //曲名
+    public Image targetImage;//画像
+    public Sprite newSprite;//新
     public string SongName;//曲名
-    public MusicPlay musicPlay;//曲を流すスクリプトへアクセス
+    public MusicPlay musicPlay;//曲saisei
     public GameObject AudioSourceObject;//オーディオソースオブジェクト
     AudioSource audioSource;
 
@@ -20,6 +20,12 @@ public class MusicSelectButton : MonoBehaviour
     public diffButton hardButton;
     public ButtonSceneChange buttonSceneChange;
     public DiffButtonAnimation diffButtonAnimation;
+    public ImageSwitcher imageSwitcherEasy;
+    public ImageSwitcher imageSwitcherNormal;
+    public ImageSwitcher imageSwitcherHard;
+
+    [SerializeField] private PlayButton playButton;
+    [SerializeField] private VariableMemory variableMemory;
 
 
     public void onClick()
@@ -30,6 +36,11 @@ public class MusicSelectButton : MonoBehaviour
         changeMusic();
         changeDiffButtons();
         diffButtonAnimation.diffAnimation();
+        imageSwitcherEasy.resetImage();
+        imageSwitcherNormal.resetImage();
+        imageSwitcherHard.resetImage();
+        variableMemory.hasExecuted = false;
+        playButton.ImageReset(); 
     }
 
     void changeText()
